575a9a06d199a9a398e8a4bcb9b5f8e2a38da5a5
Maxim Popov <maximpop1@yandex.ru>
2022-05-03 23:43:23 +0300

    changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
+require("@nomiclabs/hardhat-waffle");
+require("solidity-coverage");
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY;
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.7.3",
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`${ROPSTEN_PRIVATE_KEY}`]
+    }
+  }
 };
